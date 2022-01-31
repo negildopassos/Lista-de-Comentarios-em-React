ed97b7934d009e4ab32e3eaea59a2b7b0d471207
Negildo Passos <negildojr@gmail.com>
2022-01-31 13:25:08 -0300

    Primeiros Componentes

---
+++ src/components/Comentario.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Comentario.css'
+
+const Comentario = () => {
+    return(  
+    <div className="Comentario">
+         <div>Olá eu sou um componente</div>
+    </div> 
+    )
+}
+
+export default Comentario;
